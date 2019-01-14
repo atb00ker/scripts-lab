4c68a41e39b2fdd5d1f625e02707056704c20fed
Ajay Tripathi <ajay39in@gmail.com>
2019-01-14 15:30:15 +0530

    string-reduction: complete
    Related https://www.hackerrank.com/challenges/string-reduction/problem

--- algorithm/dynamic-programming/string-reduction.cpp
+++ algorithm/dynamic-programming/string-reduction.cpp
@@ -6,7 +6,6 @@
 
 using namespace std;
 
-// Brute Attempt #2
 char otherElement(char one, char two)
 {
     if (one == 'a')
@@ -18,17 +17,35 @@ char otherElement(char one, char two)
 
 int stringReduction(string gstring)
 {
-    char compare = gstring[0];
-    int gstring_size = gstring.size();
-    int seen_before = 1;
+    int acount, bcount, ccount, response, gstring_size, gstring_size_end, glength;
+    gstring_size = gstring_size_end = glength = gstring.size();
+    acount = bcount = ccount = 0;
+    int seen_before_end, seen_before;
+    seen_before_end = seen_before = 1;
+    char compare_end, compare = gstring[0];
+    compare_end = gstring[--glength];
     for (int i = 1; gstring[i] != '\0'; i++)
     {
-        if (compare == gstring[i])
+        if (gstring[i] == 'a')
+            acount++;
+        if (gstring[i] == 'b')
+            bcount++;
+        if (gstring[i] == 'c')
+            ccount++;
+        if (compare_end == gstring[glength - i])
+            seen_before_end++;
+        else
         {
-            seen_before++;
-            continue;
+            gstring_size_end -= seen_before_end;
+            if (seen_before_end % 2 == 0)
+                compare_end = gstring[glength - i];
+            else
+                compare_end = otherElement(compare_end, gstring[glength - i]);
+            seen_before_end = 1;
         }
-        if (seen_before > 1)
+        if (compare == gstring[i])
+            seen_before++;
+        else
         {
             gstring_size -= seen_before;
             if (seen_before % 2 == 0)
@@ -36,46 +53,29 @@ int stringReduction(string gstring)
             else
                 compare = otherElement(compare, gstring[i]);
             seen_before = 1;
-            continue;
         }
-        gstring_size--;
-        compare = otherElement(compare, gstring[i]);
     }
-    return gstring_size;
-}
-
-int trueReduction(string gstring)
-{
-    int as, bs, cs;
-    as = bs = cs = 0;
-    for (int i = 0; gstring[i] != '\0'; i++)
-    {
-        if (gstring[i] == 'a')
-            as++;
-        if (gstring[i] == 'b')
-            bs++;
-        if (gstring[i] == 'c')
-            cs++;
-    }
-    if ((as == 0 && bs == 0) || (cs == 0 && bs == 0) || (as == 0 && cs == 0))
-        return gstring.size();
-    else if ((as % 2 == 0 && bs % 2 == 0 && cs % 2 == 0) || (as % 2 != 0 && bs % 2 != 0 && cs % 2 != 0))
+    if (gstring_size < gstring_size_end)
+        response = gstring_size;
+    else
+        response = gstring_size_end;
+    if (response < 3 || acount == glength || bcount == glength || ccount == glength)
+        return response;
+    if (response > 2 && response % 2 == 0)
         return 2;
     return 1;
 }
 
 int main()
 {
-    // srand(time(NULL));
-    // string gstring, options = "abc";
-    // for (int j = 0; j < 100; j++)
-    // {
-    //     gstring.clear();
-    //     for (int i = 0; i <= 10; i++)
-    //         gstring.push_back(options[rand() % (3)]);
-    //     if (stringReduction(gstring) != trueReduction(gstring))
-    //         cout << gstring << endl;
-    // }
-    cout << stringReduction("baacbacabbb") << trueReduction("baacbacabbb");
+    srand(time(NULL));
+    string gstring, options = "abc";
+    for (int j = 0; j < 10; j++)
+    {
+        gstring.clear();
+        for (int i = 0; i <= 10; i++)
+            gstring.push_back(options[rand() % (3)]);
+        cout << stringReduction(gstring) << " " << gstring << endl;
+    }
     return 0;
 }
