e53bb8e0cc1731d173c4abdfbba4e0e7a88990c1
Ajay Tripathi <ajay39in@gmail.com>
2019-01-31 17:33:17 +0530

    abbreviations: #5
    Leaving it pending for now, until i get
    better with Dynamic programming. Attaching
    the working solution.
    
    Related https://www.hackerrank.com/challenges/abbr/problem

--- algorithm/dynamic-programming/abbreviations.cpp
+++ algorithm/dynamic-programming/abbreviations.cpp
@@ -1,4 +1,4 @@
-// Question : https: //www.hackerrank.com/challenges/abbr/problem
+// Question : https://www.hackerrank.com/challenges/abbr/problem
 
 #include <bits/stdc++.h>
 
@@ -90,15 +90,57 @@ string abbreviation(string a, string b)
     return "NO";
 }
 
+void solution(string s, string t)
+{
+    int ssize = s.size() + 1, tsize = t.size() + 1;
+    bool mat[ssize][tsize];
+    memset(mat, false, sizeof(mat));
+    mat[0][0] = 1;
+    for (int i = 1; i < ssize; i++)
+    {
+        if (islower(s[i - 1]))
+            mat[i][0] = true;
+        else
+            break;
+    }
+    for (int i = 1; i < ssize; i++)
+        for (int j = 1; j < tsize; j++)
+        {
+            if (isupper(s[i - 1]))
+            {
+                if (s[i - 1] == t[j - 1] && mat[i - 1][j - 1])
+                    mat[i][j] = true;
+                else
+                    mat[i][j] = false;
+            }
+            else
+            {
+                if (toupper(s[i - 1]) == t[j - 1] && mat[i - 1][j - 1])
+                    mat[i][j] = true;
+                else
+                    mat[i][j] = mat[i - 1][j];
+            }
+        }
+    for (int i = 1; i < ssize; i++)
+    {
+        for (int j = 1; j < tsize; j++)
+            cout << mat[i][j] << " ";
+        cout << endl;
+    }
+    if (mat[ssize - 1][tsize - 1])
+        cout << "\nYES\n";
+    else
+        cout << "\nNO\n";
+}
+
 int main()
 {
     cout << abbreviation("daBcddaBcddaBcddaBcd", "ABCABCABCABC");
     cout << abbreviation("daBcddaBcddaBcddaBcd", "ABCABCABCABC");
     cout << abbreviation("daBcddaBcd", "ABCABC");
-    cout << abbreviation("daBcddaBcd", "ABCABC");
-    cout << abbreviation("daBcd", "ABC");
-    cout << abbreviation("daBcd", "ABC");
-
     cout << abbreviation("lyylyyllyyylllyylyyylyllylyllllllyyylyllyyyylllllylyylyyllylyylllyhyllllyylllyllylyllylyllllyylylylyyylyllyyylylllyylylllyyllyylylyyllyylyyylllyllylyyllyllllyylylyylllllllyllyyyyyylllyyylylylylyyyyyyyymylyyyylyyyylyyyylyyyylylylylylyllylyylllyllyylylyyllyyyylylllyyyyyllllllyllyylllylyylyllllyyllllylyyyyyllllylylllyyyylyylyyyllyylyyyylylyyyylyyyyylyylllyyllylyyllyllyyyyyylylllylyyyyyllyylyyyyylyyylyylyylylylyyllllyylllyylylllyllyylylyllylllyllyyyyyylyyyllyllyyllyllyylyllyllyyylyyyyylylllyyylllyyyllylyllylylyyllylllylyyyyylyyyylyyyylyyyyylylllllyylyylyyyylyylyyylyylllllllyyyyyyyylyyylylllllylyrlyylllyylylllllylyylyylyyllylyyyyllyyyllylllyllylllylyyyyylylylyyllyyyyylllyyyllllylyllyyyllllyyllyyylllylyylyyyllllyllylllylyllylllyyllllyllyyymyylylllyylllllllyyyyylyyyllyyyyyyylylylyylylyylylyyllyyyllylylyyyylyyyyyyyyyyylllylylllllylllyylllyyllllllyylllllyllyyllyylyyllllyylyylyyllllyyyllllyyylylylylyylyllylllyyylylylylyyylyllllllylyllllyylyllylllyllyylylllylllyllllylyyylylllyyylllyylllllllyllyyy", "LYYLYYLYYYLLLYYLYYYYLLYLLLLLLYYYLYLLYYYYLLLLYLYLYYLLYLYLLYYLLLYYLLLLYLYLLYLYLLLLYYLYLLYYLYLLYLLLLYLYLLYLYYLLYYLLYYLYYYLLLYLYLYYLLYLLYYYLYLLLLLLLYLLYYYYYLLLYYYLYLYLYLYYYYYYYLYYYLYYYYLYYLYYYYLYYLYLYLYLLYLYYLLLYLLYYLYLYLLYYYLYLLLYYYYLLLLLLYLLYYLLLYLYYLYLLLLYLLLYLYYYYYLLLLLYLLLYYYYLYYLYYLLYYLYYYYLYLYYYYLYYYYYLYYLLLYYLLYLYLLYLLYYYYYLYLLYLYYYYYLLYYLYYYYLYYYLYYLYYLYLYLYYLLLLYYLLLYYLYLLLYLLYLYLYLLYLLLYLLYYYYYYLYYYLLYLYYLLYLLYLYLLYLLYYYLYYYYLLLLYYYLLLYYYLLYLYLLYLYLYYLLYLLLYLYYYYYLYYYYLYYYYLYYYYYLYLLLLLYYLYYLYYYLYYYYYLYYLLLLLLLYYYYYYYYLYYLYLLLLYLYLYYLLYYLYLLLLLYLYYLYYLYLLYLYYYLYYYLYLLLYLLYLLYLYYYYYLLYYYLLYYYYYLLYYYLLLLYLYLLYYYLLLLYYLLYYYLLLYLYYLYYYLLLYLLYLYLYLLYLLYYLLLYLLYYYYYLYLLLYYLLLLLLLYYYYYLYYLLYYYYYYLYYLLYYLYLYYLLYYYLLYYLYYYYLYYYYYYYYYYYLLLYYLLLLLYLLLYYLLLYYLLLLLYYLLLLYLYYLLYYLYYLLLYYLYYLYYLLLLYYYLLLLYYYYLYLYLYYYLLYLLLYYYLYLYLYLYYLYLLLLLYLYLLLYYYLLYLYLLYYLYLLYLLLYLLYLYYYLYLLLYYLLYYLLLLLLYLYY");
+    solution("daBcddaBcd", "ABCABC");
+    solution("daBcd", "ABC");
+    solution("daBcd", "ABC");
     return 0;
 }
