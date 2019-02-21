f8b682f6bb3615e2e13a02d5c829d17cecc37e6c
Ajay Tripathi <ajay39in@gmail.com>
2019-02-21 12:56:42 +0530

    fraudulent-activity-notifications: complete
    Related https://www.hackerrank.com/challenges/fraudulent-activity-notifications/problem

--- algorithm/sorting/fraudulent-activity-notifications.py
+++ algorithm/sorting/fraudulent-activity-notifications.py
@@ -1,40 +1,62 @@
 #!/bin/python3
 # Question: https://www.hackerrank.com/challenges/fraudulent-activity-notifications/problem
+# Note: Testcase 5 purchased
 
 import bisect
 
 
-def getMedian(half, sortedForMedian):
-    if not len(sortedForMedian) % 2:
-        return (sortedForMedian[half - 1] + sortedForMedian[half]) / 2.0
-    return sortedForMedian[half]
+def getMedian(d, half, countingSort, expenditure):
+    if not d % 2:
+        li, lo = None, None
+        for i in range(201):
+            if countingSort[i] >= half - 1 and not li:
+                li = i
+            if countingSort[i] >= half and not lo:
+                lo = i
+            if li and lo:
+                return (li + lo)
+    else:
+        for i in range(201):
+            if half <= countingSort[i]:
+                return (i * 2)
+
+
+def changeCountingSort(countingSort, addValue, removeValue):
+    if removeValue > addValue:
+        for i in range(addValue, removeValue):
+            countingSort[i] += 1
+    elif removeValue < addValue:
+        for i in range(removeValue, addValue):
+            countingSort[i] -= 1
 
 
 def activityNotifications(expenditure, d):
     alerts = 0
+    expLen = len(expenditure)
     half = d // 2
-
+    countingSort = [0] * 201
     # Sorting #1
-    sortedForMedian = sorted(expenditure[0:d-1])
-    # Check Alerts #1
-    nextDay = expenditure[d]
-    medianSoFar = getMedian(half, sortedForMedian)
-    if medianSoFar*2 <= nextDay:
+    for i in expenditure[0:d]:
+        countingSort[i] += 1
+    sumSoFar = -1
+    for i in range(201):
+        sumSoFar += countingSort[i]
+        countingSort[i] = sumSoFar
+    # Check #1
+    medianSoFar = getMedian(d, half, countingSort, expenditure)
+    if medianSoFar <= expenditure[d]:
         alerts += 1
 
-    for i in range(len(expenditure)-d-1):
-        # Sorting
-        sortedForMedian.remove(expenditure[i])
-        bisect.insort_left(sortedForMedian, nextDay)
-        # Check Alerts
-        nextDay = expenditure[d+i+1]
-        medianSoFar = getMedian(half, sortedForMedian)
-        if medianSoFar*2 <= nextDay:
+    for i in range(d, expLen-1):
+        removeValue = expenditure[i-d]
+        addValue = expenditure[i]
+        changeCountingSort(countingSort, addValue, removeValue)
+        medianSoFar = getMedian(d, half, countingSort, expenditure)
+        if medianSoFar <= expenditure[i+1]:
             alerts += 1
-
     return alerts
 
 
 if __name__ == '__main__':
-    # print(activityNotifications([10, 20, 30, 40, 50], 3))
+    print(activityNotifications([10, 20, 30, 40, 50], 3))
     print(activityNotifications([1, 2, 3, 4, 4], 4))
