47d0beae8ddb545c41efa427e3bfa42af9ae4cde
Ajay Tripathi <ajay39in@gmail.com>
2019-03-18 00:30:32 +0530

    [script] US Visa early appointment

--- scripts/README.md
+++ scripts/README.md
@@ -3,3 +3,4 @@
 - SHA1: Implimentation of the SHA1 using C
 - spam-filter: (ML101) Basic Spam Filter Models made by LinearSVC, Naive Bayes, Logistic Regression using the sciket-learn library.
 - telegram-bots: Contains a demo TelegramBot; ATB00kerbot, this code can be used for reference for building bots using the [python-telegram-bots](https://github.com/python-telegram-bot/python-telegram-bot) wrapper.
+- USVisaEarlyAppointment:  Application for getting early US visa interview: The tool will Scrape the CGI website and check available date before the current appointment date, if a date is available, the program will beep.
--- scripts/USVisaEarlyAppointment.py
+++ scripts/USVisaEarlyAppointment.py
@@ -1,12 +1,25 @@
+#!/bin/python3
+# Application for getting early US visa interview:
+# The tool will Scrape the CGI website and check
+# available date before the current appointment date,
+# if a date is available, the program will beep.
+
+# NOTE: SET THESE GLOBAL VARIABLES BEFORE USE
+# COOKIE: After you login, there is a `cookie`
+#         header send in your request, paste
+#         the value of that variable here.
+# CURRENT_APPOINTMENT_DATE: Date you've currently have for embassy.
+# CURRENT_VAC_DATE: Date you current have for VAC appointment.
+
 import subprocess
 import time
 import os
 
 
 # For users to change
-CURRENT_APPOINTMENT_DATE = "March 11, 2019"
-CURRENT_VAC_DATE = "February 25, 2019"
-COOKIE = "BrowserId=q8fiO_ccSrKRAE7E7e33xg; __utmc=1; oinfo=c3RhdHVzPUFDVElWRSZ0eXBlPTYmb2lkPTAwREMwMDAwMDAwUGh1UA==; autocomplete=1; lloopch_loid=00DC0000000PhuP; lloopch_lpid=060C0000000QwL9; oid=00DC0000000PhuP; apex__aa-time=Yfy4isvgW4Z8lOc8c5XgkA_3D_3D; sid_Client=h00000GQDFK0000000PhuP; clientSrc=103.204.169.224; inst=APP_0h; __utma=1.210204694.1550206513.1550670395.1550683582.17; __utmz=1.1550683582.17.13.utmcsr=ustraveldocs.com|utmccn=(referral)|utmcmd=referral|utmcct=/in/in-niv-typeb1b2.asp; __utmt=1; sid=00DC0000000PhuP!ARwAQOi.bFB5.9KF9vho6mNHd2lRUGwPNVvNDu5_0wykH6g_gW8gga.r5_jaj2YrOyt2McXB3CcNst1AXg_pwsBFOtEyNH5E; __utmb=1.2.10.1550683582"
+CURRENT_APPOINTMENT_DATE = "March 22, 2019"
+CURRENT_VAC_DATE = "March 11, 2019"
+COOKIE = ""
 
 # For developer usage only
 AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.96 Safari/537.36"
