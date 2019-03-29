b065c8730a26008d3bb0b1ef25224baeab763536
Ajay Tripathi <ajay39in@gmail.com>
2019-03-29 16:19:13 +0530

    [experiment] Dockerizing OpenWISP: Proto#2

---
+++ experiment/docker/terraform/apply.tf
@@ -0,0 +1,72 @@
+resource "kubernetes_replication_controller" "openwisp" {
+  metadata {
+    name = "openwisp"
+
+    labels {
+      App = "openwisp"
+    }
+  }
+
+  spec {
+    replicas = 3
+
+    selector {
+      App = "openwisp"
+    }
+
+    template {
+      metadata {
+        labels {
+          App = "openwisp"
+        }
+      }
+
+      spec {
+        container {
+          image   = "atb00ker/ready-to-run:openwisp-web"
+          name    = "openwisp-web"
+          command = ["/opt/openwisp2/init_command.sh"]
+
+          port {
+            container_port = 8000
+          }
+
+          env {
+            name  = "DJANGO_REDIS_HOST"
+            value = "localhost"
+          }
+
+          env {
+            name  = "DJANGO_DEBUG"
+            value = "True"
+          }
+        }
+
+        container {
+          image = "redis:alpine"
+          name  = "redis"
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "openwisp" {
+  metadata {
+    name = "openwisp"
+  }
+
+  spec {
+    selector {
+      App = "${kubernetes_replication_controller.openwisp.metadata.0.labels.App}"
+    }
+
+    port {
+      port        = 8000
+      target_port = 8000
+    }
+
+    external_ips = ["192.168.1.6"]
+    type         = "LoadBalancer"
+  }
+}
---
+++ experiment/docker/terraform/provider-kubes.tf
@@ -0,0 +1,3 @@
+provider "kubernetes" {
+  host = "http://127.0.0.1:8001"
+}
