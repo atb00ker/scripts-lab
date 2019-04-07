a2f452a3f0807a5bf733f3c4539411962b925591
Ajay Tripathi <ajay39in@gmail.com>
2019-04-07 16:15:32 +0530

    [chore] clean-up

--- experiment/dockerize-openwisp/build/common/init_command.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-python manage.py collectstatic --noinput
-python manage.py migrate --noinput
-python load_init_data.py
-python manage.py runserver 0.0.0.0:${CONTAINER_PORT}
--- experiment/dockerize-openwisp/terraform/kubes-provider.tf
+++
@@ -1,6 +0,0 @@
-provider "kubernetes" {
-  host = "http://127.0.0.1:8001"
-
-  # username = "username"
-  # password = "password"
-}
