f1611868ef44a0248b7d3aa72c302d7cc88b24aa
Ajay Tripathi <ajay39in@gmail.com>
2019-07-17 20:15:11 +0530

    special-palindrome-again: complete
    Related https://www.hackerrank.com/challenges/special-palindrome-again/problem

--- algorithm/strings/special-palindrome-again.py
+++ algorithm/strings/special-palindrome-again.py
@@ -2,33 +2,44 @@
 # Question: https://www.hackerrank.com/challenges/special-palindrome-again/problem
 
 
-def isPalen(wstr):
-    wlen = (len(wstr) // 2) + 1
-    for i in range(1, wlen):
-        if wstr[:i] != wstr[-i:]:
-            return False
-    return True
-
-
-def allSubsets(s, setlen, subsets=[]):
-    i, slen = 0, len(s)
-    while i + setlen <= slen:
-        subsets.append(s[i: i + setlen])
-        i += 1
-    if setlen is not 1:
-        allSubsets(s, setlen-1, subsets)
-    return subsets
-
-
-def subSets(s):
-    count = 0
-    subsets = allSubsets(s, len(s))
-    for element in subsets:
-        if isPalen(element):
+def substrCount(n, s):
+    countList, elementList, count, prevElement, result = [], [], 0, s[0], 0
+    for index in range(n):
+        if s[index] == prevElement:
             count += 1
-    return count
+        else:
+            result += count*(count+1)/2
+            countList.append(count)
+            elementList.append(s[index-1])
+            count = 1
+        prevElement = s[index]
+    result += count*(count+1)/2
+    countList.append(count)
+    elementList.append(s[index])
+
+    print(result, countList, elementList, count)
+
+    clen = len(countList)-1
+    for index in range(1, clen):
+        if countList[index] == 1 and elementList[index-1] == elementList[index+1]:
+            print(index)
+            result += min(countList[index-1], countList[index+1])
+
+    print(result, countList, elementList, count)
+
+    return int(result)
 
 
 if __name__ == "__main__":
-    mstr = "asasd"
-    print(subSets(mstr))
+    mstr = "acbac"
+    print(substrCount(len(mstr), mstr))
+    # Assets#1
+    # import random
+    # options = "abc"
+    # while True:
+    #     mstr = ""
+    #     for _ in range(5):
+    #         mstr += options[random.randrange(3)]
+    #     if substrCount(len(mstr), mstr) != CountSpecialPalindrome(len(mstr), mstr):
+    #         print(mstr)
+    #         break
